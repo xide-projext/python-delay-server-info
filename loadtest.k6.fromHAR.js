a816dd507937c78e5b0821c34eaf30c4167c186d
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-10-24 10:05:17 +0900

    load testing with K6 from HAR

---
+++ loadtest.k6.fromHAR.js
@@ -0,0 +1,26 @@
+// Creator: k6 Browser Recorder 0.6.2
+
+import { sleep, group } from 'k6'
+import http from 'k6/http'
+
+export const options = {}
+
+export default function main() {
+  let response
+
+  group('page_1 - http://localhost:8080/', function () {
+    response = http.get('http://localhost:8080/', {
+      headers: {
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+      },
+    })
+  })
+
+  // Automatically added sleep
+  sleep(1)
+}
